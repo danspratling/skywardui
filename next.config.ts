aec2288b588d9ec7674bfabe6483255f3903d484
Dan Spratling <dan@danspratling.dev>
2025-05-24 14:02:21 +0100

    Setup (#1)
    * Setup
    
    * Update pipelines
    
    * Fix Lint errors
    
    * Fix - remove unused configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  eslint: {
+    dirs: ["pages", "app", "components", "lib", "utils", "ui"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
+}
 
 export default nextConfig;
