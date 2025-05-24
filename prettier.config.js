a11f2bb9103bd088ea01276364bff31824412f8a
Dan Spratling <dan@danspratling.dev>
2025-05-24 09:14:49 +0100

    Setup

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'always',
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: false,
+  plugins: [
+    'prettier-plugin-tailwindcss',
+    '@ianvs/prettier-plugin-sort-imports',
+  ],
+  // printWidth: 100,
+};
