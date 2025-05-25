e878ed53d8f6d3458fd23372764b53354deddb10
Dan Spratling <dan@danspratling.dev>
2025-05-25 08:30:43 +0100

    Fix up header component

--- stories/Header.tsx
+++ stories/Header.tsx
@@ -51,16 +51,16 @@ export const Header = ({
             <span className="welcome">
               Welcome, <b>{user.name}</b>!
             </span>
-            <Button size="small" onClick={onLogout} label="Log out" />
+            <Button size="sm" onClick={onLogout}>Log out</Button>
           </>
         ) : (
           <>
-            <Button size="small" onClick={onLogin} label="Log in" />
+            <Button size="sm" onClick={onLogin}>Log in</Button>
             <Button
-              primary
-              size="small"
-              onClick={onCreateAccount}
-              label="Sign up"
+              variant="primary"
+              size="sm"
+                onClick={onCreateAccount}
+                >Sign up</Button>
             />
           </>
         )}
