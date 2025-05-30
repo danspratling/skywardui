5a5c6be9cbe6d33e0d482f3e5541abf92b7736b1
Dan Spratling <dan@danspratling.dev>
2025-05-30 22:00:52 +0100

    Adds avatar component

---
+++ ui/Author/Author.d.ts
@@ -0,0 +1,10 @@
+import type { ImageProps } from "../Image/Image.d";
+
+export type AuthorProps = {
+  href?: string;
+  avatar?: ImageProps;
+  name?: string | React.ReactElement;
+  detail?: string | React.ReactElement;
+  size?: "xs" | "sm" | "md" | "lg" | "xl" | "2xl";
+  className?: string;
+};
