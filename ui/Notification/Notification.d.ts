7da4ff725c98562ac991de1aa6d7c188cd2181b8
Dan Spratling <dan@danspratling.dev>
2025-05-31 09:00:24 +0100

    Add notification

---
+++ ui/Notification/Notification.d.ts
@@ -0,0 +1,9 @@
+export type NotificationProps = {
+  count: number;
+  max?: number;
+  showNumber?: boolean;
+  showZero?: boolean;
+  showPlus?: boolean;
+  children: React.ReactNode;
+  className?: string;
+};
